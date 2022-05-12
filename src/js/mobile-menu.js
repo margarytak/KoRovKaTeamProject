13eb0f56909b9635a4f371351606879841442c80
Alona-Panchenko <81909803+Alona-Panchenko@users.noreply.github.com>
2022-05-12 11:08:39 +0300

    bugfix/headerfinal
    окно выезжает

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,5 +1,6 @@
 ;(() => {
   const menuBtnRef = document.querySelector("[data-menu-button]")
+  
   const mobileMenuRef = document.querySelector("[data-menu]")
   const body = document.querySelector("body");
 
